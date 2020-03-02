b20a5c8d833114f34d9ea6289cd7af3a719dcbb9
mateusrochar <mateusrochar@gmail.com>
2020-03-02 08:05:09 -0300

    calculadora com erro

--- Calculadora2/Program.cs
+++ Calculadora2/Program.cs
@@ -6,7 +6,189 @@ namespace Calculadora2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            operation date = new operation();
+            string verificador;
+
+            string iniciador = "start";
+
+            while (iniciador == "start")
+            {
+
+                Console.Write("Insira o primeiro numero:");
+                date.NumberA = double.Parse(Console.ReadLine().Trim());
+
+                Console.Write("Insira a operação(+;-;*;/):");
+                date.Symbol = Console.ReadLine().Trim();
+
+                Console.Write("Insira o segundo numero:");
+                date.NumberB = double.Parse(Console.ReadLine().Trim());
+
+                date.Operations(date.Symbol);
+
+                Console.WriteLine("Resultado: "+date.Result);
+
+
+                string iniciador2 = "start";
+
+                while (iniciador2 == "start")
+                {
+
+                    Console.WriteLine("Deseja continuar calculando com o resultado(y/n)?");
+                    verificador = Console.ReadLine().Trim();
+                    if (verificador == "y")
+                    {
+                        Console.Write("Insira a operação(+;-;*;/):");
+                        date.Symbol = Console.ReadLine().Trim();
+
+                        Console.Write("Insira o segundo numero:");
+                        date.NumberB = double.Parse(Console.ReadLine().Trim());
+
+                        date.Operations(date.Symbol);
+
+                        Console.WriteLine("Resultado: "+ date.Result);
+
+                    }
+
+                    else if (verificador == "n")
+                    {
+                        iniciador2 = null;
+                    }
+                }
+
+                Console.WriteLine("Deseja reiniciar ou desligar (r/d): ");
+                verificador = Console.ReadLine().Trim();
+
+
+                if (verificador == "r")
+                {
+                    date.NumberA = 0;
+                    date.NumberB = 0;
+                    date.Symbol = null;
+                    date.Result = 0;
+                }
+
+                else if (verificador == "d")
+                {
+                    iniciador = null;
+                }
+
+            }
+
+            Console.WriteLine("Fim do cálculo.");
+            Console.ReadLine();
+
+
         }
+        /*
+
+
+        double numberSum;
+        string operation;
+        string recebeNumero;
+        int totalNumbers;
+
+
+
+
+
+        // int a = 0;
+        string b;
+
+
+        for (int i = 1; i <= totalNumbers; i++)
+        {
+            Console.Write($"Entre com o {i}º numero: ");
+
+            while (true)
+            {
+                string receberNumero = Console.ReadLine();
+
+                if (double.TryParse(receberNumero.Trim(), out double a))
+                {
+
+                    numberSum.Add(a);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Entre com um numero válido: o numerno deve ter até 9 digitos, não insira letras ou espaço.");
+
+                }
+
+
+            }
+
+
+            if (i < totalNumbers)
+            {
+
+                while (true)
+                {
+                    Console.Write($"Insira o simbolo da operação que deseja realizar: +, -, /, *: ");
+                    b = Console.ReadLine();
+                    if (b.Trim() == "+" || b.Trim() == "-" || b.Trim() == "/" || b.Trim() == "*")
+                    {
+
+                        operation.Add(b);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entre com um simbolo válido.");
+                    }
+                }
+            }
+
+        }
+
+        int k = 0;
+
+        double sum = 0.0;
+
+        for (int i = 0; i < totalNumbers; i++)
+        {
+
+            if (i == 0)
+            {
+                sum += numberSum[i];
+            }
+
+            else if (operation[k] == "+")
+            {
+                sum += numberSum[i];
+                k++;
+            }
+            else if (operation[k] == "-")
+            {
+                sum -= numberSum[i];
+                k++;
+
+            }
+
+            else if (operation[k] == "*")
+            {
+                sum *= numberSum[i];
+                k++;
+
+            }
+
+
+            else if (operation[k] == "/")
+            {
+                sum /= numberSum[i];
+
+            }
+
+
+
+        }
+
+        Console.WriteLine($"O valor calculado é: {sum}");
+        Console.ReadLine();
+
+  */
     }
 }
+
---
+++ Calculadora2/operation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora2
+{
+    class operation
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+        public double Result { get; set; }
+        public string Symbol { get; set; }
+
+
+
+        public void Operations(string a)
+        {
+
+            if(a == "+")
+            {
+                Sum(NumberA, NumberB);
+            }
+            else if (a == "-")
+            {
+                Subtraction(NumberA, NumberB);
+            }
+            else if (a == "*")
+            {
+                Multiplication(NumberA, NumberB);
+            }
+            else if (a == "/")
+            {
+                Division(NumberA, NumberB);
+            }
+
+
+
+
+        }
+
+
+
+
+        public double Sum (double numberA, double numberB)
+        {
+            return numberA + numberB;
+        }
+
+        public double Subtraction (double numberA, double numberB)
+        {
+            return numberA - numberB;
+        }
+
+        public double Multiplication (double numberA, double numberB)
+        {
+            return numberA * numberB;
+        }
+
+        public double Division (double numberA, double numberB)
+        {
+            return numberA / numberB;
+        }
+
+
+    }
+}
